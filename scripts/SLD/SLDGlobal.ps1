44060bb7f226b9415cd4c35b8e0fbad77178e589
Sam Davenport <157653319+thereclaimer@users.noreply.github.com>
2025-04-11 09:47:50 -0500

    working on the memory build script

--- scripts/SLD/SLDGlobal.ps1
+++ scripts/SLD/SLDGlobal.ps1
@@ -1,23 +1,49 @@
-$script:GStaticLibFlags = 
-    "/c "    +
-    "/MD "   +
-    "/Z7 "   +
-    "/EHsc " +
-    "/std:c++17 "
+$script:StaticLibFlags = 
+    "/c"    +
+    "/MD"   +
+    "/Z7"   +
+    "/EHsc" +
+    "/std:c++17"
 
-$script:GLibRoot = $PSScriptRoot + "..\..\"
+$script:ObjDir = "build\debug\obj\"
+$script:BinDir = "build\debug\bin\"
+$script:LibDir = "build\debug\lib\"
 
-function Create-CompileCommandsStaticLib {
+$script:LibRoot = $PSScriptRoot + "..\..\"
+
+$script:CLPath   = "cl.exe "
+$script:LibPath  = "lib.exe "
+$script:LinkPath = "link.exe "
+
+$script:ObjPathFlag = "/Fo:" + $script:ObjDir 
+
+function Create-CompileCommandLib {
 	param (
-		[string]$LibName
 		[string]$LibSource,
 		[string]$LibInclude
 	)
 	
-	$directory = $script:GLibRoot + $LibName
-	$arguments = 
-		"cl.exe "               + 
-		$LibSource              + " "
+	$compileCommand = 
+		$script:CLPath          +
+		$LibSource              +
 		$script:GStaticLibFlags +
-	$source = "" 
+		$script:ObjPathFlag
+
+	return $compileCommand
+}
+
+function Create-LibCommand {
+	param (
+		[string]$LibName
+		[string]$ObjList
+	)
+
+	$outString = $script:LibDir + $LibName
+
+	$libCommand = 
+		$script:LibPath +
+		$outString      +
+		$ObjList
+
+	return $libCommand
 }
---
+++ scripts/SLD/SLDMemory.ps1
@@ -0,0 +1,3 @@
+function Create-MemoryCompileCommands {
+	
+}
